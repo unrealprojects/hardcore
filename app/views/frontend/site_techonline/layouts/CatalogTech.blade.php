31e0b73627804554801ecadc1d69b1b7d700badb
Олег <office@unrealprojects.com>
2014-09-07 16:31:32 +0300

    ->update db

--- app/views/frontend/site_techonline/layouts/CatalogTech.blade.php
+++ app/views/frontend/site_techonline/layouts/CatalogTech.blade.php
@@ -13,7 +13,6 @@
 
         <ul class="Lot-Gallery Grid-Node-1-3">
         @foreach(json_decode($list_elem['photos'],true) as $photo)
-            {{Route::getCurrentRoute()->getPath()}}
             <li><img src="/photo/techonline/{{$photo['src']}}" alt="{{$photo['name']}}"></li>
 
         @endforeach
@@ -34,12 +33,12 @@
 
 <!-- Фильтрация :: По брендам -->
 @foreach($content['brands'] as $brand)
-    <a href="/catalog/?brand={{$brand['name']}}&{{\Input::getQueryString()}}">{{$brand['name']}}</a>
+    <a href="/catalog/?brand={{$brand['alias']}}&{{\Input::getQueryString()}}">{{$brand['name']}}</a>
 @endforeach
 
 <!-- Фильтрация :: По категориям -->
 @foreach($content['categories'] as $category)
-    <a href="/catalog/?category={{$category['name']}}&{{\Input::getQueryString()}}">{{$category['name']}}</a>
+    <a href="/catalog/?category={{$category['alias']}}&{{\Input::getQueryString()}}">{{$category['name']}}</a>
 @endforeach
 
 <!-- Фильтрация :: По параметрам -->
