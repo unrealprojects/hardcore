0b4d028b1f4431ce6d864da94ccdf21d01cfc745
a <a>
2014-09-12 06:24:47 +0300

    New changelist

--- app/views/frontend/site_techonline/parts/header.blade.php
+++ app/views/frontend/site_techonline/parts/header.blade.php
@@ -4,39 +4,60 @@
     <div class="Centrate" role="banner">
 
         <div href="http://hardcore/catalog" class="Site-Logo">
-            <a href="/">
-                <div class="">
-                    <img src="/img/techonline/logo.png"/>
-                    <h2>Стройтехника
-                        <small>.Онлайн</small>
-                    </h2>
-                </div>
-            </a>
+            <div>
+                <img src="/img/techonline/logo.png"/>
+
+                <h2>Стройтехника
+                    <small>.Онлайн</small>
+                </h2>
+            </div>
+            <a class="Link-Home" href="/">Вернуться на главную страницу</a>
         </div>
+
+
+
         <nav class="Page-Navigation">
             <ul>
                 <li><a href="/catalog" title="Каталог стройтехники">Каталог</a></li>
-                <li><a href="/rent"    title="Взять стройтехнику в аренду">Аренда</a></li>
+                <li><a href="/rent" title="Взять стройтехнику в аренду">Аренда</a></li>
                 <li><a href="/parts">Запчасти и сервис</a></li>
                 <li><a href="/sellers">Арендодатели</a></li>
             </ul>
         </nav>
 
+               <div class="Page-Auth">
+                   <button class="Sign-In">
+                       Войти на сайт
+                   </button>
+                   <a href="#">Регистрация</a>        <!--
+                   <div class="Sign-In-UI">
+                       <form class="Form-Horizontal" action="">
+                           <h4>Авторизация</h4>
+                           <div class="Control-Group">
+                               <label for="Sign-In-Username">Пользователь</label>
+                               <input id="Sign-In-Username" type="text"/>
+                           </div>
 
-        <!--<div class="Page-Auth">
-            <button class="Sign-In">
-                Войти на сайт
-            </button>
-            <p>Впервые на сайте?<br>
-                <a href="#">Регистрация</a>
-            </p>
-        </div>
-        <!-- Авторизованный пользователь
-       <div class="Page-Auth">
-           <a href="#">Константин Константинопольский</a>
-           <a class="User-Exit" href="#">Выйти</a>
-       </div>
-       -->
+                           <div class="Control-Group">
+                               <label for="Sign-In-Password">Пароль</label>
+                               <input id="Sign-In-Password" type="password"/>
+                           </div>
+                           <label class="Control-Group Checkbox Submit" for="Sign-In-Remember-Me">
+                               <div class="Grid-Node-1-2">
+                                   <input id="Sign-In-Remember-Me" type="checkbox"/>Запомнить меня
+                               </div>
+                               <input class="class="Grid-Node-1-2"" type="submit" value="Войти на сайт"/>
+                           </label>
+
+                       </form>
+                   </div>
+               </div>
+               Авторизованный пользователь
+              <div class="Page-Auth">
+                  <a href="#">Константин Константинопольский</a>
+                  <a class="User-Exit" href="#">Выйти</a>
+              </div>
+              -->
 
     </div>
 
