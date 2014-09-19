a1318e0c4e399c30b74efa095628d30a96705cc0
Олег <office@unrealprojects.com>
2014-09-19 12:42:42 +0300

    filter

--- app/views/general/scripts_ie8.blade.php
+++ app/views/general/scripts_ie8.blade.php
@@ -1,15 +1,10 @@
 <!--[if lt IE 9]>
-<script>
-    document.createElement('header');
-    document.createElement('nav');
-    document.createElement('section');
-    document.createElement('article');
-    document.createElement('aside');
-    document.createElement('footer');
-    document.createElement('main');
-</script>
-
-<script src="/js/general/libs/ie8/jquery.placeholder.js" type="text/javascript"></script>
+<script src="/js/general/libs/ie8/html5shiv.js" type="text/javascript"></script>
 <script src="/js/general/libs/ie8/respond.js" type="text/javascript"></script>
 
+
+<![endif]-->
+
+<!--[if lt IE 10]>
+<script src="/js/general/libs/ie8/jquery.placeholder.js" type="text/javascript"></script>
 <![endif]-->
