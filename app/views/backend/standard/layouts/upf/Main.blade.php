127a15d9695b10e23f37a3e3c7b466438ea6cb0c
a <a>
2014-09-22 22:43:04 +0300

    New changelist

--- app/views/backend/standard/layouts/upf/Main.blade.php
+++ app/views/backend/standard/layouts/upf/Main.blade.php
@@ -4,8 +4,14 @@
 <aside class="Menu">
     <ul class="Menu-Category">
         <li class="Menu-Category-Item">
-            <a class="Menu-Category-Link" href="#"><span class="fa fa-pencil-square-o"></span>Публикации</a><span class="Menu-Category-Icon fa fa-angle-right"></span>
+            <a class="Menu-Category-Link" href="#">
+                <span class="fa fa-dashboard"></span>Приборная панель
+            </a>
+        </li>
+        <li class="Menu-Category-Item">
+            <a class="Menu-Category-Link Active" href="#"><span class="fa fa-pencil-square-o"></span>Публикации</a><span class="Menu-Category-Icon fa fa-angle-right"></span>
             <ul class="Menu-Subcategory">
+                <li class="Menu-Subcategory-Item"><a class="Menu-Subcategory-Link Active" href="#"><span class="fa fa-pencil"></span>Создать</a></li>
                 <li class="Menu-Subcategory-Item"><a class="Menu-Subcategory-Link" href="#"><span class="fa fa-file"></span>Страницы</a></li>
                 <li class="Menu-Subcategory-Item"><a class="Menu-Subcategory-Link" href="#"><span class="fa fa-calendar"></span>Новости</a></li>
                 <li class="Menu-Subcategory-Item"><a class="Menu-Subcategory-Link" href="#"><span class="fa fa-pencil"></span>Черновики</a></li>
@@ -34,4 +40,32 @@
     </ul>
 </aside>
 <div class="Menu-Shadow"></div>
+<main>
+    <nav class="Userbar">
+        <ul>
+            <li><a href="#"><span class="fa fa-bars fa-lg"></span></a></li>
+            <li><a href="#"><span class="fa fa-home fa-lg"></span></a></li>
+        </ul>
+    </nav>
+    <nav class="Breadcrumbs">
+        <ul class="Breadcrumb-List">
+            <li class="Breadcrumb-Item"itemscope itemtype="http://data-vocabulary.org/Breadcrumb">
+                <a href="/" itemprop="url">
+                    <span itemprop="title">Главная</span>
+                </a>
+            </li>
+            <li class="Breadcrumb-Item"itemscope itemtype="http://data-vocabulary.org/Breadcrumb">
+                <a href="/" itemprop="url">
+                    <span itemprop="title">Публикации</span>
+                </a>
+            </li>
+            <li class="Breadcrumb-Item">
+                <span>Создать</span>
+            </li>
+        </ul>
+    </nav>
+    <section class="Content">
+        <h3 class="Heading Secondary">Создание новой публикации</h3>
+    </section>
+</main>
 @endsection
