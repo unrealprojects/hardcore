4199ed7cc8be6d0aebe6374977eb90c60adc616e
a <a>
2014-09-23 22:15:06 +0300

    New changelist

--- app/views/frontend/site_techonline/layouts/MainPage.blade.php
+++ app/views/frontend/site_techonline/layouts/MainPage.blade.php
@@ -11,59 +11,7 @@
     </div>
 </section>
 <section class="Node">
-    <table class="Solid Lines Stripped Edit">
-        <thead>
-        <tr>
-            <td><input type="checkbox"/></td>
-            <td>№</td>
-            <td>Заголовок</td>
-            <td>Содержание</td>
-            <td>Опубликовано</td>
-            <td>Действия</td>
-        </tr>
-        </thead>
-        <tbody>
-        <tr>
-            <td><input type="checkbox"/></td>
-            <td class="Number">01</td>
-            <td contenteditable="true">Заголовок статьи или страницы</td>
-            <td contenteditable="true" class="Main">Строительство качественных автомагистралей, областных и городских дорог не может выполняться без использования грейдеров.</td>
-            <td contenteditable="true">16 августа 2014</td>
-            <td class="Action">
-                <ul>
-                    <li><a title="Принять изменения" href="#"><span class="fa fa-check"></span></a></li>
-                    <li><a title="Удалить ..." href="#"><span class="fa fa-remove"></span></a></li>
-                </ul>
-            </td>
-        </tr>
-        <tr>
-            <td><input type="checkbox"/></td>
-            <td class="Number">02</td>
-            <td contenteditable="true">Заголовок статьи или страницы</td>
-            <td contenteditable="true" class="Main">Строительство качественных автомагистралей, областных и городских дорог не может выполняться без использования грейдеров.</td>
-            <td contenteditable="true">26 августа 2014</td>
-            <td class="Action">
-                <ul>
-                    <li><a title="Принять изменения" href="#"><span class="fa fa-check"></span></a></li>
-                    <li><a title="Удалить ..." href="#"><span class="fa fa-remove"></span></a></li>
-                </ul>
-            </td>
-        </tr>
-        <tr>
-            <td><input type="checkbox"/></td>
-            <td class="Number">03</td>
-            <td contenteditable="true">Другой заголовок статьи или страницы</td>
-            <td contenteditable="true" class="Main">Строительство качественных автомагистралей, областных и городских дорог не может выполняться без использования грейдеров.</td>
-            <td contenteditable="true">30 августа 2014</td>
-            <td class="Action">
-                <ul>
-                    <li><a title="Принять изменения" href="#"><span class="fa fa-check"></span></a></li>
-                    <li><a title="Удалить ..." href="#"><span class="fa fa-remove"></span></a></li>
-                </ul>
-            </td>
-        </tr>
-        </tbody>
-    </table>
+
 </section>
 <!-- ФИЛЬТР -->
 @include('frontend.site_techonline.layouts.filter.MainPageFilter');
